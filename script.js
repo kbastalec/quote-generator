8b18043b9120ce8e12352dd28b92e8ce4f30b956
kbastalec <kristian.bastalec@gmail.com>
2021-08-13 10:08:57 +0200

    Improving readability of the code

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
-// Show Loading
-function loading () {
+
+function showLoadingSpinner () {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-// Hide Loading
-function complete() {
+
+function removeLoadingSpinner() {
     loader.hidden = true;
     quoteContainer.hidden = false; 
 }
 
 // Show New Quote
 function newQuote() {
-    loading();
+    showLoadingSpinner();
 //Pick a radnom quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if Author field is blank and replace it with 'Unknown'
@@ -40,12 +40,12 @@ function newQuote() {
 
     // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
-    complete();
+    removeLoadingSpinner();
 }
 
 // Get Quotes From API
 async function getQuotes() {
-    loading();
+    showLoadingSpinner();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
